2eea2b8920af3a1aa67a3591c3301a4fa368cd47
MgKyaw <mgkyaw20112@gmail.com>
2025-11-04 22:19:40 +0700

    feat: Add sample code for Map

--- Map.js
+++ Map.js
@@ -19,3 +19,15 @@ map.delete("b");
 
 console.log(map.size);
 // Expected output: 2
+
+/////////// Setting object properties
+
+const contacts = new Map();
+contacts.set("Jessie", { phone: "213-555-1234", address: "123 N 1st Ave" });
+contacts.has("Jessie"); // true
+contacts.get("Hilary"); // undefined
+contacts.set("Hilary", { phone: "617-555-4321", address: "321 S 2nd St" });
+contacts.get("Jessie"); // {phone: "213-555-1234", address: "123 N 1st Ave"}
+contacts.delete("Raymond"); // false
+contacts.delete("Jessie"); // true
+console.log(contacts.size); // 1
