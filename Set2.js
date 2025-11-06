98d00899bd7c484d3a5a3f9d0382085116bf2a4d
MgKyaw <mgkyaw20112@gmail.com>
2025-11-06 23:38:41 +0700

    feat: Add Iterating sets

--- Set2.js
+++ Set2.js
@@ -26,3 +26,52 @@ mySet1.size; // 4, since we just removed one value
 mySet1.add(5); // Set(5) { 1, 'some text', {...}, {...}, 5 } - a previously deleted item will be added as a new item, it will not retain its original position before deletion
 
 console.log(mySet1); // Set(5) { 1, "some text", {…}, {…}, 5 }
+
+
+for (const item of mySet1) {
+  console.log(item);
+}
+// 1, "some text", { "a": 1, "b": 2 }, { "a": 1, "b": 2 }, 5
+
+for (const item of mySet1.keys()) {
+  console.log(item);
+}
+// 1, "some text", { "a": 1, "b": 2 }, { "a": 1, "b": 2 }, 5
+
+for (const item of mySet1.values()) {
+  console.log(item);
+}
+// 1, "some text", { "a": 1, "b": 2 }, { "a": 1, "b": 2 }, 5
+
+// key and value are the same here
+for (const [key, value] of mySet1.entries()) {
+  console.log(key);
+}
+// 1, "some text", { "a": 1, "b": 2 }, { "a": 1, "b": 2 }, 5
+
+// Convert Set object to an Array object, with Array.from
+const myArr = Array.from(mySet1); // [1, "some text", {"a": 1, "b": 2}, {"a": 1, "b": 2}, 5]
+
+// the following will also work if run in an HTML document
+mySet1.add(document.body);
+mySet1.has(document.querySelector("body")); // true
+
+// converting between Set and Array
+const mySet2 = new Set([1, 2, 3, 4]);
+console.log(mySet2.size); // 4
+console.log([...mySet2]); // [1, 2, 3, 4]
+
+// intersect can be simulated via
+const intersection = new Set([...mySet1].filter((x) => mySet2.has(x)));
+
+// difference can be simulated via
+const difference = new Set([...mySet1].filter((x) => !mySet2.has(x)));
+
+// Iterate set entries with forEach()
+mySet2.forEach((value) => {
+  console.log(value);
+});
+// 1
+// 2
+// 3
+// 4
