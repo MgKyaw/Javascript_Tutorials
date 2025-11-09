6674a93b0833c10c85943c2e831aed7b2decd0f5
MgKyaw <mgkyaw20112@gmail.com>
2025-11-09 15:26:28 +0700

    feat: Add using the WeakSet object

---
+++ WeakSet.js
@@ -0,0 +1,15 @@
+//// Using the WeakSet object
+
+const ws = new WeakSet();
+const foo = {};
+const bar = {};
+
+ws.add(foo);
+ws.add(bar);
+
+ws.has(foo); // true
+ws.has(bar); // true
+
+ws.delete(foo); // removes foo from the set
+ws.has(foo); // false, foo has been removed
+ws.has(bar); // true, bar is retained
