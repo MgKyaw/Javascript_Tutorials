5301313a792a418b1ec927d841f2ce81e034fa65
MgKyaw <mgkyaw20112@gmail.com>
2025-11-09 15:27:27 +0700

    feat: Add WeakSet s are ideal for this purpose.

--- WeakSet.js
+++ WeakSet.js
@@ -13,3 +13,33 @@ ws.has(bar); // true
 ws.delete(foo); // removes foo from the set
 ws.has(foo); // false, foo has been removed
 ws.has(bar); // true, bar is retained
+
+//// WeakSet s are ideal for this purpose.
+
+// Execute a callback on everything stored inside an object
+function execRecursively(fn, subject, _refs = new WeakSet()) {
+  // Avoid infinite recursion
+  if (_refs.has(subject)) {
+    return;
+  }
+
+  fn(subject);
+  if (typeof subject === "object" && subject) {
+    _refs.add(subject);
+    for (const key in subject) {
+      execRecursively(fn, subject[key], _refs);
+    }
+    _refs.delete(subject);
+  }
+}
+
+const foo = {
+  foo: "Foo",
+  bar: {
+    bar: "Bar",
+  },
+};
+
+foo.bar.baz = foo; // Circular reference!
+execRecursively((obj) => console.log(obj), foo);
+
