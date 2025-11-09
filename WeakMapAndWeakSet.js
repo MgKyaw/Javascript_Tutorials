c7a51e3a81764f3bc71bbe21ff62f44bcfd658fd
MgKyaw <mgkyaw20112@gmail.com>
2025-11-09 20:33:09 +0700

    feat: Add Example 2

--- WeakMapAndWeakSet.js
+++ WeakMapAndWeakSet.js
@@ -7,3 +7,14 @@ john = null;
 
 // the object will be removed from memory
 
+//// Example 2
+
+let john2 = { name: "John" };
+
+let array = [ john2 ];
+
+john2 = null; // overwrite the reference
+
+// the object previously referenced by john is stored inside the array
+// therefore it won't be garbage-collected
+// we can get it as array[0]
