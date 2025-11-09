e27c4c1410b209eae201dac5c8a45ffaae6f7715
MgKyaw <mgkyaw20112@gmail.com>
2025-11-09 20:33:49 +0700

    feat: Add Map example

--- WeakMapAndWeakSet.js
+++ WeakMapAndWeakSet.js
@@ -18,3 +18,14 @@ john2 = null; // overwrite the reference
 // the object previously referenced by john is stored inside the array
 // therefore it won't be garbage-collected
 // we can get it as array[0]
+
+//// Map example
+let john3 = { name: "John" };
+
+let map = new Map();
+map.set(john3, "...");
+
+john3 = null; // overwrite the reference
+
+// john is stored inside the map,
+// we can get it by using map.keys()
