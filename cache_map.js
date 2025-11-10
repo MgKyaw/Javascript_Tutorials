08814e15d4fcb4cbbf1246201ff3ffea905fb4fc
MgKyaw <mgkyaw20112@gmail.com>
2025-11-10 20:43:04 +0700

    feat: Update cache example with WeakMap

--- cache_map.js
+++ cache_map.js
@@ -1,10 +1,40 @@
+// // 📁 cache.js
+// let cache = new Map();
+
+// // calculate and remember the result
+// function process(obj) {
+//   if (!cache.has(obj)) {
+//     let result = /* calculations of the result for */ obj;
+
+//     cache.set(obj, result);
+//     return result;
+//   }
+
+//   return cache.get(obj);
+// }
+
+// // Now we use process() in another file:
+
+// // 📁 main.js
+// let obj = {/* let's say we have an object */};
+
+// let result1 = process(obj); // calculated
+
+// // ...later, from another place of the code...
+// let result2 = process(obj); // remembered result taken from cache
+
+// // ...later, when the object is not needed any more:
+// obj = null;
+
+// alert(cache.size); 
+
 // 📁 cache.js
-let cache = new Map();
+let cache = new WeakMap();
 
 // calculate and remember the result
 function process(obj) {
   if (!cache.has(obj)) {
-    let result = /* calculations of the result for */ obj;
+    let result = /* calculate the result for */ obj;
 
     cache.set(obj, result);
     return result;
@@ -13,17 +43,15 @@ function process(obj) {
   return cache.get(obj);
 }
 
-// Now we use process() in another file:
-
 // 📁 main.js
-let obj = {/* let's say we have an object */};
+let obj = {/* some object */};
 
-let result1 = process(obj); // calculated
-
-// ...later, from another place of the code...
-let result2 = process(obj); // remembered result taken from cache
+let result1 = process(obj);
+let result2 = process(obj);
 
 // ...later, when the object is not needed any more:
 obj = null;
 
-alert(cache.size); 
+// Can't get cache.size, as it's a WeakMap,
+// but it's 0 or soon be 0
+// When obj gets garbage collected, cached data will be removed as well
