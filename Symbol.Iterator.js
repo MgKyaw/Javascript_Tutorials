831aad95dcaac9108b34b097db5124bf551574f7
MgKyaw <mgkyaw20112@gmail.com>
2025-11-12 20:52:20 +0700

    feat: Use Symbol.iterator

--- Symbol.Iterator.js
+++ Symbol.Iterator.js
@@ -6,3 +6,10 @@ for (let num of numbers) {
 // 1
 // 2
 // 3
+
+var iterator = numbers[Symbol.iterator]();
+
+console.log(iterator.next()); // Object {value: 1, done: false}
+console.log(iterator.next()); // Object {value: 2, done: false}
+console.log(iterator.next()); // Object {value: 3, done: false}
+console.log(iterator.next()); // Object {value: undefined, done: true}
