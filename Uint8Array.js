4129db3dfd64c50d312e89ea4fe38c37c6926423
MgKyaw <mgkyaw20112@gmail.com>
2025-11-13 21:04:40 +0700

    feat: Add toBinary method

--- Uint8Array.js
+++ Uint8Array.js
@@ -13,3 +13,23 @@ uint8[true] = 0;
 console.log(uint8[true]); // 0
 
 Object.freeze(uint8); // TypeError: Cannot freeze array buffer views with elements
+
+function toBinary(
+  x,
+  { type = "Float64", littleEndian = false, separator = " ", radix = 16 } = {},
+) {
+  const bytesNeeded = globalThis[`${type}Array`].BYTES_PER_ELEMENT;
+  const dv = new DataView(new ArrayBuffer(bytesNeeded));
+  dv[`set${type}`](0, x, littleEndian);
+  const bytes = Array.from({ length: bytesNeeded }, (_, i) =>
+    dv
+      .getUint8(i)
+      .toString(radix)
+      .padStart(8 / Math.log2(radix), "0"),
+  );
+  return bytes.join(separator);
+}
+
+console.log(toBinary(1.1)); // 3f f1 99 99 99 99 99 9a
+console.log(toBinary(1.1, { littleEndian: true })); // 9a 99 99 99 99 99 f1 3f
+console.log(toBinary(20, { type: "Int8", radix: 2 })); // 00010100
