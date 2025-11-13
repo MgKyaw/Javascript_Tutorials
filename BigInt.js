b7451bedf37aae86499c5c71bc36a5786f49aac7
MgKyaw <mgkyaw20112@gmail.com>
2025-11-13 19:51:41 +0700

    feat: Add special cases

--- BigInt.js
+++ BigInt.js
@@ -21,3 +21,16 @@ typeof 1n === "bigint"; // true
 typeof BigInt("1") === "bigint"; // true
 
 typeof Object(1n) === "object"; // true
+
+//// Special cases
+
+const previousMaxSafe = BigInt(Number.MAX_SAFE_INTEGER); // 9007199254740991n
+const maxPlusOne = previousMaxSafe + 1n; // 9007199254740992n
+const theFuture = previousMaxSafe + 2n; // 9007199254740993n, this works now!
+const prod = previousMaxSafe * 2n; // 18014398509481982n
+const diff = prod - 10n; // 18014398509481972n
+const mod = prod % 10n; // 2n
+const bigN = 2n ** 54n; // 18014398509481984n
+bigN * -1n; // -18014398509481984n
+const expected = 4n / 2n; // 2n
+const truncated = 5n / 2n; // 2n, not 2.5n
