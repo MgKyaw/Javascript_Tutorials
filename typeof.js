6d5776680f4a405ab79756382d0d142ba3e1573e
MgKyaw <mgkyaw20112@gmail.com>
2025-11-15 10:55:18 +0700

    feat: Print the type of functions

--- typeof.js
+++ typeof.js
@@ -20,3 +20,14 @@ typeof "Hello world"; // returns "string"
 typeof document.lastModified; // returns "string"
 typeof window.length; // returns "number"
 typeof Math.LN2; // returns "number"
+
+typeof blur; // returns "function"
+typeof parseInt; // returns "function"
+typeof shape.split; // returns "function"
+
+typeof Date; // returns "function"
+typeof Function; // returns "function"
+typeof Math; // returns "object"
+typeof Option; // returns "function"
+typeof String; // returns "function"
+
