00015c2ba6c6b7c22faa59b2344d1ac46b7a1b08
MgKyaw <mgkyaw20112@gmail.com>
2025-11-15 10:51:08 +0700

    feat: Add typeof example

---
+++ typeof.js
@@ -0,0 +1,12 @@
+const myFun = () => 5 + 2;
+const shape = "round";
+const size = 1;
+const foo = ["Apple", "Mango", "Orange"];
+const today = new Date();
+
+typeof myFun; // returns "function"
+typeof shape; // returns "string"
+typeof size; // returns "number"
+typeof foo; // returns "object"
+typeof today; // returns "object"
+typeof doesntExist; // returns "undefined"
