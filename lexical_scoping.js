282a626f0804294aafa69810f550bf4e9e38540f
MgKyaw <mgkyaw20112@gmail.com>
2025-11-16 15:55:59 +0700

    feat: Add makeAdder function

--- lexical_scoping.js
+++ lexical_scoping.js
@@ -28,3 +28,16 @@ function makeFunc() {
 const myFunc = makeFunc();
 myFunc();
 
+//// makeAdder function
+function makeAdder(x) {
+  return function (y) {
+    return x + y;
+  };
+}
+
+const add5 = makeAdder(5);
+const add10 = makeAdder(10);
+
+console.log(add5(2)); // 7
+console.log(add10(2)); // 12
+
