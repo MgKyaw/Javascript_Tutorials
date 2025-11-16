1236bbd0d99e99194b05312fe77cbd8e535b2528
MgKyaw <mgkyaw20112@gmail.com>
2025-11-16 15:45:34 +0700

    feat: Add relational operator

--- typeof.js
+++ typeof.js
@@ -31,3 +31,23 @@ typeof Math; // returns "object"
 typeof Option; // returns "function"
 typeof String; // returns "function"
 
+//// Relational operators
+// Arrays
+const trees = ["redwood", "bay", "cedar", "oak", "maple"];
+0 in trees; // returns true
+3 in trees; // returns true
+6 in trees; // returns false
+"bay" in trees; // returns false
+// (you must specify the index number, not the value at that index)
+"length" in trees; // returns true (length is an Array property)
+
+// built-in objects
+"PI" in Math; // returns true
+const myString = new String("coral");
+"length" in myString; // returns true
+
+// Custom objects
+const myCar = { make: "Honda", model: "Accord", year: 1998 };
+"make" in myCar; // returns true
+"model" in myCar; // returns true
+
