479811d20e702cb0e6c9a8d0da1fa273ac6635bd
MgKyaw <mgkyaw20112@gmail.com>
2025-11-17 21:12:50 +0700

    feat: Call loadScript method with callback function

--- callback.js
+++ callback.js
@@ -14,3 +14,9 @@ function loadScript(src, callback) {
 
   document.head.append(script);
 }
+
+loadScript('/my/script.js', function() {
+  // the callback runs after the script is loaded
+  newFunction(); // so now it works
+
+});
