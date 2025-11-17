f59524613405dcea4af9c9f3b277ce3dd05dec7d
MgKyaw <mgkyaw20112@gmail.com>
2025-11-17 21:12:10 +0700

    feat: Add callback parameter into loadScript method

--- callback.js
+++ callback.js
@@ -1,7 +1,16 @@
-function loadScript(src) {
-  // creates a <script> tag and append it to the page
-  // this causes the script with given src to start loading and run when complete
+// function loadScript(src) {
+//   // creates a <script> tag and append it to the page
+//   // this causes the script with given src to start loading and run when complete
+//   let script = document.createElement('script');
+//   script.src = src;
+//   document.head.append(script);
+// }
+
+function loadScript(src, callback) {
   let script = document.createElement('script');
   script.src = src;
+
+  script.onload = () => callback(script);
+
   document.head.append(script);
 }
