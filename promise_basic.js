75aaf8d91ada79bc42f98a8e086ac9ae25bd7d40
MgKyaw <mgkyaw20112@gmail.com>
2025-11-18 20:52:23 +0700

    feat: Add new promise with .then method

--- promise_basic.js
+++ promise_basic.js
@@ -8,3 +8,11 @@ timerPromise.then(function(value) {
    console.log(value);
 });
 
+new Promise(function(resolve, reject) {
+   setTimeout(function() {
+      resolve('Hello');
+   }, 3000);
+})
+.then(function(value) {
+   console.log(value);
+});
