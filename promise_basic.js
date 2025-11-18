61b4b6a7c3375f985c2d27635f6b807d328ad368
MgKyaw <mgkyaw20112@gmail.com>
2025-11-18 20:53:33 +0700

    feat: Add delay method

--- promise_basic.js
+++ promise_basic.js
@@ -16,3 +16,14 @@ new Promise(function(resolve, reject) {
 .then(function(value) {
    console.log(value);
 });
+
+function delay(time) {
+   var d = new Date();
+   while (new Date() - d < time);
+}
+
+var promise = new Promise(function(resolve, reject) {
+   delay(5000); // Delay the page render for 5 seconds
+   console.log("OK!")
+});
+
