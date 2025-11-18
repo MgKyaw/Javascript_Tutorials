792dd71c1e17c59617f061652d48ab17014d79b5
MgKyaw <mgkyaw20112@gmail.com>
2025-11-18 20:47:46 +0700

    feat: Add new callback function

--- promise_introduction.js
+++ promise_introduction.js
@@ -9,3 +9,19 @@ function getRequest(url, callback) {
    xhr.send();
 }
 
+function getJSON(url, callback, errorCallback) {
+   var xhr = new XMLHttpRequest();
+   xhr.onreadystatechange = function(e) {
+      if (this.state === 4 && this.status === 200) {
+         try {
+            var json = JSON.parse(this.responseText);
+            callback(json);
+         }
+         catch (e) {
+            errorCallback(e);
+         }
+      }
+   }
+   xhr.open('GET', url, false);
+   xhr.send();
+}
