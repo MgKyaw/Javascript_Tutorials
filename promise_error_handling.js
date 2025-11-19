e8eaeaade76263aa0d7f96a4b0296e9f6da8fd57
MgKyaw <mgkyaw20112@gmail.com>
2025-11-19 20:14:35 +0700

    feat: Add new Promise

--- promise_error_handling.js
+++ promise_error_handling.js
@@ -4,3 +4,8 @@ function Promise(executor) {
     catch(e) { reject(e); }
 }
 
+var p = new Promise(function(resolve, reject) {
+    setTimeout(function() {
+        reject("Sorry");
+    }, 3000);
+});
