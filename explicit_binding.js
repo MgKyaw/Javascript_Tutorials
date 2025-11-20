9c68a9f8340dd1db8121bb7201ca79d14e753d0f
MgKyaw <mgkyaw20112@gmail.com>
2025-11-20 19:36:29 +0700

    feat: Add nested function

--- explicit_binding.js
+++ explicit_binding.js
@@ -16,3 +16,17 @@ const obj = {
 
 obj.myFunction();   
 // {someKey: 1, myFunction: ƒ}. ie. obj
+
+//// Nested Function
+const obj2 = {
+  someKey: 1, 
+  outer: function() {
+    function inner(){
+       console.log(this);
+    }     
+    inner();
+  },
+}
+
+obj2.outer();      // Window {}
+
