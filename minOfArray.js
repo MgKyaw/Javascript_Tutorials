e3dacab306f0864c67a348df2a987a6490eb9ac4
MgKyaw <mgkyaw20112@gmail.com>
2025-11-22 13:39:44 +0700

    feat: Add minOfArray method with apply method

---
+++ minOfArray.js
@@ -0,0 +1,16 @@
+function minOfArray(arr) {
+  let min = Infinity;
+  const QUANTUM = 32768;
+
+  for (let i = 0; i < arr.length; i += QUANTUM) {
+    const subMin = Math.min.apply(
+      null,
+      arr.slice(i, Math.min(i + QUANTUM, arr.length)),
+    );
+    min = Math.min(subMin, min);
+  }
+
+  return min;
+}
+
+const min = minOfArray([5, 6, 2, 3, 7]);
