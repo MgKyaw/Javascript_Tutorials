50d02aa51feb9eadfcafd08c434af301fbbd6001
MgKyaw <mgkyaw20112@gmail.com>
2025-11-23 10:29:54 +0700

    feat: Add Base.bind()

---
+++ base_bind.js
@@ -0,0 +1,10 @@
+class Base {
+  constructor(...args) {
+    console.log(new.target === Base);
+    console.log(args);
+  }
+}
+
+const BoundBase = Base.bind(null, 1, 2);
+
+new BoundBase(3, 4); // true, [1, 2, 3, 4]
