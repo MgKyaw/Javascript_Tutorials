35aa2c2170cf41e795780421c2ac64edf39306a6
MgKyaw <mgkyaw20112@gmail.com>
2025-11-23 10:26:40 +0700

    feat: prevent `this` from being boxed into the wrapper object

--- bind_example.js
+++ bind_example.js
@@ -17,3 +17,12 @@ bind(thisArg)
 bind(thisArg, arg1)
 bind(thisArg, arg1, arg2)
 bind(thisArg, arg1, arg2, /* …, */ argN)
+
+"use strict"; // prevent `this` from being boxed into the wrapper object
+
+function log(...args) {
+  console.log(this, ...args);
+}
+const boundLog = log.bind("this value", 1, 2);
+const boundLog2 = boundLog.bind("new this value", 3, 4);
+boundLog2(5, 6); // "this value", 1, 2, 3, 4, 5, 6
