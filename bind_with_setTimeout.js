9c568fa59bf0040d85c7d20ad3f4c13831554fac
MgKyaw <mgkyaw20112@gmail.com>
2025-11-23 10:40:13 +0700

    feat: Add bind function with setTimeout

---
+++ bind_with_setTimeout.js
@@ -0,0 +1,16 @@
+class LateBloomer {
+  constructor() {
+    this.petalCount = Math.floor(Math.random() * 12) + 1;
+  }
+  bloom() {
+    // Declare bloom after a delay of 1 second
+    setTimeout(this.declare.bind(this), 1000);
+  }
+  declare() {
+    console.log(`I am a beautiful flower with ${this.petalCount} petals!`);
+  }
+}
+
+const flower = new LateBloomer();
+flower.bloom();
+// After 1 second, calls 'flower.declare()'
