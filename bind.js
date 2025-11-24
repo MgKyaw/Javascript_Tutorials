a261ea8606eeee7079ac9e97de870a9f9f3c396e
MgKyaw <mgkyaw20112@gmail.com>
2025-11-24 20:41:42 +0700

    feat: Use and call user.say.bind with parameter

--- bind.js
+++ bind.js
@@ -11,22 +11,34 @@
 
 // funcUser("Hello"); // Hello, John (argument "Hello" is passed, and this=user)
 
+// let user = {
+//   firstName: "John",
+//   sayHi() {
+//     alert(`Hello, ${this.firstName}!`);
+//   }
+// };
+
+// let sayHi = user.sayHi.bind(user); // (*)
+
+// // can run it without an object
+// sayHi(); // Hello, John!
+
+// setTimeout(sayHi, 1000); // Hello, John!
+
+// // even if the value of user changes within 1 second
+// // sayHi uses the pre-bound value which is reference to the old user object
+// user = {
+//   sayHi() { alert("Another user in setTimeout!"); }
+// };
+
 let user = {
   firstName: "John",
-  sayHi() {
-    alert(`Hello, ${this.firstName}!`);
+  say(phrase) {
+    alert(`${phrase}, ${this.firstName}!`);
   }
 };
 
-let sayHi = user.sayHi.bind(user); // (*)
+let say = user.say.bind(user);
 
-// can run it without an object
-sayHi(); // Hello, John!
-
-setTimeout(sayHi, 1000); // Hello, John!
-
-// even if the value of user changes within 1 second
-// sayHi uses the pre-bound value which is reference to the old user object
-user = {
-  sayHi() { alert("Another user in setTimeout!"); }
-};
+say("Hello"); // Hello, John! ("Hello" argument is passed to say)
+say("Bye"); // Bye, John! ("Bye" is passed to say)
