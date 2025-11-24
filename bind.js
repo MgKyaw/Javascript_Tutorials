c4660c54c95749eb4418bf483d15438f180e003c
MgKyaw <mgkyaw20112@gmail.com>
2025-11-24 20:39:55 +0700

    feat: Add example code with bind function

--- bind.js
+++ bind.js
@@ -1,12 +1,32 @@
+// let user = {
+//   firstName: "John"
+// };
+
+// function func() {
+//   alert(this.firstName);
+// }
+
+// let funcUser = func.bind(user);
+// funcUser(); // John
+
+// funcUser("Hello"); // Hello, John (argument "Hello" is passed, and this=user)
+
 let user = {
-  firstName: "John"
+  firstName: "John",
+  sayHi() {
+    alert(`Hello, ${this.firstName}!`);
+  }
 };
 
-function func() {
-  alert(this.firstName);
-}
+let sayHi = user.sayHi.bind(user); // (*)
 
-let funcUser = func.bind(user);
-funcUser(); // John
+// can run it without an object
+sayHi(); // Hello, John!
 
-funcUser("Hello"); // Hello, John (argument "Hello" is passed, and this=user)
+setTimeout(sayHi, 1000); // Hello, John!
+
+// even if the value of user changes within 1 second
+// sayHi uses the pre-bound value which is reference to the old user object
+user = {
+  sayHi() { alert("Another user in setTimeout!"); }
+};
