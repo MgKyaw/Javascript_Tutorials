e7fde1377c5ec4743b1a1da367a76465226ad715
MgKyaw <mgkyaw20112@gmail.com>
2025-11-25 21:37:49 +0700

    feat: Add mul.bind() and call it

--- partial_function.js
+++ partial_function.js
@@ -1,5 +1,11 @@
-let bound = func.bind(context, [arg1], [arg2], ...);
+// let bound = func.bind(context, [arg1], [arg2], ...);
 
 function mul(a, b) {
   return a * b;
 }
+
+let double = mul.bind(null, 2);
+
+alert( double(3) ); // = mul(2, 3) = 6
+alert( double(4) ); // = mul(2, 4) = 8
+alert( double(5) ); // = mul(2, 5) = 10
