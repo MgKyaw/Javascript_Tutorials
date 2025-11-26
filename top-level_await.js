14cffdac4b26f4ddebfdd9b0247302cce3db21e9
MgKyaw <mgkyaw20112@gmail.com>
2025-11-26 20:15:15 +0700

    feat: Add anonymous async function

--- top-level_await.js
+++ top-level_await.js
@@ -4,3 +4,8 @@ let user = await response.json();
 
 console.log(user);
 
+(async () => {
+  let response = await fetch('/article/promise-chaining/user.json');
+  let user = await response.json();
+  
+})();
