9c73ba33191725740681168da1b2b750da99078b
MgKyaw <mgkyaw20112@gmail.com>
2025-11-26 20:21:56 +0700

    feat: Use try catch in async function

--- async_error_handling.js
+++ async_error_handling.js
@@ -1,8 +1,21 @@
-async function f() {
-  await Promise.reject(new Error("Whoops!"));
-}
+// async function f() {
+//   await Promise.reject(new Error("Whoops!"));
+// }
 
 //is the same as this:
 // async function f() {
 //   throw new Error("Whoops!");
 // }
+
+async function f() {
+
+  try {
+    let response = await fetch('/no-user-here');
+    let user = await response.json();
+  } catch(err) {
+    // catches errors both in fetch and response.json
+    alert(err);
+  }
+}
+
+f();
