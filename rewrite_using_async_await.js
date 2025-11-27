e63966090b5318f0c6921a2325918b7f9b976027
MgKyaw <mgkyaw20112@gmail.com>
2025-11-27 20:49:10 +0700

    feat: Update function with async/await

--- rewrite_using_async_await.js
+++ rewrite_using_async_await.js
@@ -1,13 +1,24 @@
-function loadJson(url) {
-  return fetch(url)
-    .then(response => {
-      if (response.status == 200) {
-        return response.json();
-      } else {
-        throw new Error(response.status);
-      }
-    });
+// function loadJson(url) {
+//   return fetch(url)
+//     .then(response => {
+//       if (response.status == 200) {
+//         return response.json();
+//       } else {
+//         throw new Error(response.status);
+//       }
+//     });
+// }
+
+async function loadJson(url) {
+  const response = await fetch(url);
+
+  if (response.status === 200) {
+    return response.json();
+  } else {
+    throw new Error(response.status);
+  }
 }
+
 
 loadJson('https://javascript.info/no-such-user.json')
   .catch(alert); // Error: 404
