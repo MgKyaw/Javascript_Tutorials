2aafab502fefea750ca289a95d788765c4b20cfb
MgKyaw <mgkyaw20112@gmail.com>
2025-11-30 11:03:56 +0700

    feat: Add fetch call with try/catch

--- AjaxBattel/fetch.js
+++ AjaxBattel/fetch.js
@@ -3,3 +3,11 @@ fetch("/service", { method: "GET" })
   .then((json) => console.log(json))
   .catch((err) => console.error("error:", err));
 
+try {
+  const res = await fetch("/service", { method: "GET" }),
+    json = await res.json();
+
+  console.log(json);
+} catch (err) {
+  console.error("error:", err);
+}
