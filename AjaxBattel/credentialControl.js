c6e8551ecd67ef40d6e89d614ca12acc04524e34
MgKyaw <mgkyaw20112@gmail.com>
2025-12-01 21:28:03 +0700

    feat: Add document for CredentialControl

--- AjaxBattel/credentialControl.js
+++ AjaxBattel/credentialControl.js
@@ -2,3 +2,10 @@ const res = await fetch("/service", {
   method: "GET",
   credentials: "same-origin",
 });
+
+// credentials can be set to:
+
+// 'omit' - exclude cookies and HTTP authentication entries (the default)
+// 'same-origin' - include credentials with requests to same-origin URLs
+// 'include' - include credentials on all requests
+// Note that include was the default in earlier API implementations. Explicitly set the credentials property if your users are likely to run older browsers.
