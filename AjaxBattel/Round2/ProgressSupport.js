b2dddd6d1e80c286f83d3821b594dcd04573d3a7
MgKyaw <mgkyaw20112@gmail.com>
2025-12-02 20:57:50 +0700

    feat: Add document for Progress Support

--- AjaxBattel/Round2/ProgressSupport.js
+++ AjaxBattel/Round2/ProgressSupport.js
@@ -4,3 +4,10 @@ const xhr = new XMLHttpRequest();
 xhr.upload.onprogress = (p) => {
   console.log(Math.round((p.loaded / p.total) * 100) + "%");
 };
+
+// The event handler is passed an object with three properties:
+
+// lengthComputable - set true if the progress can be calculated
+// total - the total amount of work - or Content-Length - of the message body
+// loaded - the amount of work - or content - completed so far
+// The Fetch API does not offer any way to monitor upload progress.
