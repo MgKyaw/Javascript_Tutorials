a9068096646ffa85bbdb5c5e8a7e258eca0ed998
MgKyaw <mgkyaw20112@gmail.com>
2025-12-02 21:34:13 +0700

    feat: Call fetch with request object

--- Fetch/FetchMDNDocs/creatingARequestObject.js
+++ Fetch/FetchMDNDocs/creatingARequestObject.js
@@ -1,8 +1,10 @@
 const myHeaders = new Headers();
 myHeaders.append("Content-Type", "application/json");
 
-const response = await fetch("https://example.org/post", {
+const myRequest = new Request("https://example.org/post", {
   method: "POST",
   body: JSON.stringify({ username: "example" }),
   headers: myHeaders,
 });
+
+const response = await fetch(myRequest);
