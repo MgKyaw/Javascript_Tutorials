05da110e9f43b03dad31c9363a3eaa552ee4515f
MgKyaw <mgkyaw20112@gmail.com>
2025-12-02 21:33:19 +0700

    feat: Add creating a Request object

---
+++ Fetch/FetchMDNDocs/creatingARequestObject.js
@@ -0,0 +1,8 @@
+const myHeaders = new Headers();
+myHeaders.append("Content-Type", "application/json");
+
+const response = await fetch("https://example.org/post", {
+  method: "POST",
+  body: JSON.stringify({ username: "example" }),
+  headers: myHeaders,
+});
