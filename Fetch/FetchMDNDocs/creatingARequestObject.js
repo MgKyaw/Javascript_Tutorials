920cb8ae6a5f2bde312bf0b57a77371ab3061bde
MgKyaw <mgkyaw20112@gmail.com>
2025-12-02 21:36:18 +0700

    feat: Add sample code for Fetch method with Request parameter

--- Fetch/FetchMDNDocs/creatingARequestObject.js
+++ Fetch/FetchMDNDocs/creatingARequestObject.js
@@ -8,3 +8,28 @@ const myRequest = new Request("https://example.org/post", {
 });
 
 const response = await fetch(myRequest);
+
+async function post(request) {
+  try {
+    const response = await fetch(request);
+    const result = await response.json();
+    console.log("Success:", result);
+  } catch (error) {
+    console.error("Error:", error);
+  }
+}
+
+const request1 = new Request("https://example.org/post", {
+  method: "POST",
+  headers: {
+    "Content-Type": "application/json",
+  },
+  body: JSON.stringify({ username: "example1" }),
+});
+
+const request2 = new Request(request1, {
+  body: JSON.stringify({ username: "example2" }),
+});
+
+post(request1);
+post(request2);
