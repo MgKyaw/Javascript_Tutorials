cc85e90aac0df94e8b0d630bec144aaea3806661
MgKyaw <mgkyaw20112@gmail.com>
2025-12-03 23:28:38 +0700

    feat: Add document for reading the response body

--- Fetch/FetchMDNDocs/reading_the_response_body.js
+++ Fetch/FetchMDNDocs/reading_the_response_body.js
@@ -1,3 +1,14 @@
+// The Response interface provides a number of methods to retrieve the entire body contents in a variety of different formats:
+
+// Response.arrayBuffer()
+// Response.blob()
+// Response.formData()
+// Response.json()
+// Response.text()
+// These are all asynchronous methods, returning a Promise which will be fulfilled with the body content.
+
+// In this example, we fetch an image and read it as a Blob, which we can then use to create an object URL:
+
 const image = document.querySelector("img");
 
 const url = "flowers.jpg";
