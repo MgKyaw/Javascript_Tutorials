b3115ecac23a61cf6e91522fe2b5cbe01ea8a722
MgKyaw <mgkyaw20112@gmail.com>
2025-12-04 21:37:31 +0700

    feat: Add example code

--- Fetch/NetworkRequest_Fetch/fetch.js
+++ Fetch/NetworkRequest_Fetch/fetch.js
@@ -1,3 +1,14 @@
 // The basic syntax is:
 
 let promise = fetch(url, [options])
+
+// For example:
+
+let response = await fetch(url);
+
+if (response.ok) { // if HTTP-status is 200-299
+  // get the response body (the method explained below)
+  let json = await response.json();
+} else {
+  alert("HTTP-Error: " + response.status);
+}
