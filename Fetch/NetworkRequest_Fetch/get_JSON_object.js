20a60fe2874dbe82b946eaf37ed7fddfae27f130
MgKyaw <mgkyaw20112@gmail.com>
2025-12-04 21:58:47 +0700

    feat: Add the alternative way with promise

--- Fetch/NetworkRequest_Fetch/get_JSON_object.js
+++ Fetch/NetworkRequest_Fetch/get_JSON_object.js
@@ -6,3 +6,9 @@ let response = await fetch(url);
 let commits = await response.json(); // read response body and parse as JSON
 
 alert(commits[0].author.login);
+
+// Or, the same without await, using pure promises syntax:
+
+fetch('https://api.github.com/repos/javascript-tutorial/en.javascript.info/commits')
+  .then(response => response.json())
+  .then(commits => alert(commits[0].author.login));
