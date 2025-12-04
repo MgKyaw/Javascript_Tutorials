c8f733dc9b7477f091419a12520fd511539fe0fc
MgKyaw <mgkyaw20112@gmail.com>
2025-12-04 21:57:58 +0700

    feat: Add get JSON-object

---
+++ Fetch/NetworkRequest_Fetch/get_JSON_object.js
@@ -0,0 +1,8 @@
+// let’s get a JSON-object with latest commits from GitHub:
+
+let url = 'https://api.github.com/repos/javascript-tutorial/en.javascript.info/commits';
+let response = await fetch(url);
+
+let commits = await response.json(); // read response body and parse as JSON
+
+alert(commits[0].author.login);
