3635f3632be4e42ec204875125a8fe19b98310c4
MgKyaw <mgkyaw20112@gmail.com>
2025-12-04 21:33:30 +0700

    feat: Implement an offline cache with service workers

--- Fetch/FetchMDNDocs/Response.clone.js
+++ Fetch/FetchMDNDocs/Response.clone.js
@@ -16,3 +16,28 @@ async function getData() {
     console.error(error.message);
   }
 }
+
+// This is a common pattern when implementing an offline cache with service workers. The service worker wants to return the response to the app, but also to cache the response. So it clones the response, returns the original, and caches the clone:
+
+async function cacheFirst(request) {
+  const cachedResponse = await caches.match(request);
+  if (cachedResponse) {
+    return cachedResponse;
+  }
+  try {
+    const networkResponse = await fetch(request);
+    if (networkResponse.ok) {
+      const cache = await caches.open("MyCache_1");
+      cache.put(request, networkResponse.clone());
+    }
+    return networkResponse;
+  } catch (error) {
+    return Response.error();
+  }
+}
+
+self.addEventListener("fetch", (event) => {
+  if (precachedResources.includes(url.pathname)) {
+    event.respondWith(cacheFirst(event.request));
+  }
+});
