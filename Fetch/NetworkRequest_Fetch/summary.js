8efbe1a6628bda77700a868e8661870097fe63a1
MgKyaw <mgkyaw20112@gmail.com>
2025-12-05 22:09:00 +0700

    feat: Add example code without await

--- Fetch/NetworkRequest_Fetch/summary.js
+++ Fetch/NetworkRequest_Fetch/summary.js
@@ -2,3 +2,10 @@
 
 let response = await fetch(url, options); // resolves with response headers
 let result = await response.json(); // read body as json
+
+// Or, without await:
+
+fetch(url, options)
+  .then(response => response.json())
+  .then(result => console.log("result"))
+
