4d796be2468e6a8db8bfa1e98a90854c66c9042c
MgKyaw <mgkyaw20112@gmail.com>
2025-12-05 22:00:00 +0700

    feat: Add getting await response.text()

--- Fetch/NetworkRequest_Fetch/get_JSON_object.js
+++ Fetch/NetworkRequest_Fetch/get_JSON_object.js
@@ -12,3 +12,11 @@ alert(commits[0].author.login);
 fetch('https://api.github.com/repos/javascript-tutorial/en.javascript.info/commits')
   .then(response => response.json())
   .then(commits => alert(commits[0].author.login));
+
+// To get the response text, await response.text() instead of .json():
+
+let response2 = await fetch('https://api.github.com/repos/javascript-tutorial/en.javascript.info/commits');
+
+let text = await response2.text(); // read response body as text
+
+alert(text.slice(0, 80) + '...');
