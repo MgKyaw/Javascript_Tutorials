59af4a2ecab7427c3c4e200173273ba841cabec5
MgKyaw <mgkyaw20112@gmail.com>
2025-12-05 22:09:43 +0700

    feat: Add documentation for summary

--- Fetch/NetworkRequest_Fetch/summary.js
+++ Fetch/NetworkRequest_Fetch/summary.js
@@ -9,3 +9,20 @@ fetch(url, options)
   .then(response => response.json())
   .then(result => console.log("result"))
 
+// Response properties:
+
+// response.status – HTTP code of the response,
+// response.ok – true if the status is 200-299.
+// response.headers – Map-like object with HTTP headers.
+// Methods to get response body:
+
+// response.text() – return the response as text,
+// response.json() – parse the response as JSON object,
+// response.formData() – return the response as FormData object (multipart/form-data encoding, see the next chapter),
+// response.blob() – return the response as Blob (binary data with type),
+// response.arrayBuffer() – return the response as ArrayBuffer (low-level binary data),
+// Fetch options so far:
+
+// method – HTTP-method,
+// headers – an object with request headers (not any header is allowed),
+// body – the data to send (request body) as string, FormData, BufferSource, Blob or UrlSearchParams object.
