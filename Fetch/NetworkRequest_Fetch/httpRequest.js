d7467d9dfd483dbb162c5728fa38722e802204b0
MgKyaw <mgkyaw20112@gmail.com>
2025-12-06 23:47:08 +0700

    feat: Handle binary data

--- Fetch/NetworkRequest_Fetch/httpRequest.js
+++ Fetch/NetworkRequest_Fetch/httpRequest.js
@@ -6,3 +6,11 @@ const req = new XMLHttpRequest();
 req.addEventListener("load", reqListener);
 req.open("GET", "http://www.example.org/example.txt");
 req.send();
+
+// Handling binary data
+
+const req2 = new XMLHttpRequest();
+req2.open("GET", url);
+// retrieve data unprocessed as a binary string
+req2.overrideMimeType("text/plain; charset=x-user-defined");
+/* … */
