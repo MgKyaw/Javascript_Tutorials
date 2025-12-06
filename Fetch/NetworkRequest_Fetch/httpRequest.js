f4c7346da0f9ca762b86401bd9ccaec9982d0e97
MgKyaw <mgkyaw20112@gmail.com>
2025-12-06 23:48:09 +0700

    feat: Add arraybuffer example

--- Fetch/NetworkRequest_Fetch/httpRequest.js
+++ Fetch/NetworkRequest_Fetch/httpRequest.js
@@ -14,3 +14,15 @@ req2.open("GET", url);
 // retrieve data unprocessed as a binary string
 req2.overrideMimeType("text/plain; charset=x-user-defined");
 /* … */
+
+// arraybuffer
+
+const req3 = new XMLHttpRequest();
+
+req3.onload = (e) => {
+  const arraybuffer = req.response; // not responseText
+  /* … */
+};
+req3.open("GET", url);
+req3.responseType = "arraybuffer";
+req3.send();
