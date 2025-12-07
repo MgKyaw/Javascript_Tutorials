d7269fb996f60dba3c999d89737495741984891c
MgKyaw <mgkyaw20112@gmail.com>
2025-12-07 23:28:00 +0700

    feat: Add document for initialize request

--- Fetch/NetworkRequest_HttpRequest/TheBasic.js
+++ Fetch/NetworkRequest_HttpRequest/TheBasic.js
@@ -1,3 +1,10 @@
 let xhr = new XMLHttpRequest();
 
 xhr.open(method, URL, [async, user, password])
+
+// This method specifies the main parameters of the request:
+
+// method – HTTP-method. Usually "GET" or "POST".
+// URL – the URL to request, a string, can be URL object.
+// async – if explicitly set to false, then the request is synchronous, we’ll cover that a bit later.
+// user, password – login and password for basic HTTP auth (if required).
