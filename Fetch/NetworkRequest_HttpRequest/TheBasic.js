510f093b30bf47445be370c580ad599c5bc9b6db
MgKyaw <mgkyaw20112@gmail.com>
2025-12-07 23:29:18 +0700

    feat: Add document for listening to xhr events

--- Fetch/NetworkRequest_HttpRequest/TheBasic.js
+++ Fetch/NetworkRequest_HttpRequest/TheBasic.js
@@ -10,3 +10,11 @@ xhr.open(method, URL, [async, user, password])
 // user, password – login and password for basic HTTP auth (if required).
 
 xhr.send([body])
+
+// Listen to xhr events for response.
+
+// These three events are the most widely used:
+
+// load – when the request is complete (even if HTTP status is like 400 or 500), and the response is fully downloaded.
+// error – when the request couldn’t be made, e.g. network down or invalid URL.
+// progress – triggers periodically while the response is being downloaded, reports how much has been downloaded.
