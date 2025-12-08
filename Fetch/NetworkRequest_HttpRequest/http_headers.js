473460c870161719f7bfdd4388dcfe47d786a2bb
MgKyaw <mgkyaw20112@gmail.com>
2025-12-08 23:44:44 +0700

    feat: Get all response headers

--- Fetch/NetworkRequest_HttpRequest/http_headers.js
+++ Fetch/NetworkRequest_HttpRequest/http_headers.js
@@ -7,3 +7,14 @@ xhr.setRequestHeader('X-Auth', '456');
 // X-Auth: 123, 456
 
 xhr.getResponseHeader('Content-Type')
+
+let headers = xhr
+  .getAllResponseHeaders()
+  .split('\r\n')
+  .reduce((result, current) => {
+    let [name, value] = current.split(': ');
+    result[name] = value;
+    return result;
+  }, {});
+
+// headers['Content-Type'] = 'image/png'
