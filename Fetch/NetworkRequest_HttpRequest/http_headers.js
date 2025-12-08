8a61ef3b146a6c3db2826a11244ce97fe285ecf8
MgKyaw <mgkyaw20112@gmail.com>
2025-12-08 23:43:59 +0700

    feat: set and get headers

--- Fetch/NetworkRequest_HttpRequest/http_headers.js
+++ Fetch/NetworkRequest_HttpRequest/http_headers.js
@@ -1,1 +1,9 @@
 xhr.setRequestHeader('Content-Type', 'application/json');
+
+xhr.setRequestHeader('X-Auth', '123');
+xhr.setRequestHeader('X-Auth', '456');
+
+// the header will be:
+// X-Auth: 123, 456
+
+xhr.getResponseHeader('Content-Type')
