caa3b2d0e2697713469386d8e13a7183e6d2ff54
MgKyaw <mgkyaw20112@gmail.com>
2025-12-08 23:45:49 +0700

    feat: Add document

--- Fetch/NetworkRequest_HttpRequest/post_formdata.js
+++ Fetch/NetworkRequest_HttpRequest/post_formdata.js
@@ -1,2 +1,7 @@
 let formData = new FormData([form]); // creates an object, optionally fill from <form>
 formData.append(name, value); // appends a field
+
+// We create it, optionally fill from a form, append more fields if needed, and then:
+
+// xhr.open('POST', ...) – use POST method.
+// xhr.send(formData) to submit the form to the server.
