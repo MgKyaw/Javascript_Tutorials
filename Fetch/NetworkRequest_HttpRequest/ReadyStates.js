b1123bf1c9bb0e7aea51b04872be53f22fd296a6
MgKyaw <mgkyaw20112@gmail.com>
2025-12-08 23:40:01 +0700

    feat: Add Ready states

---
+++ Fetch/NetworkRequest_HttpRequest/ReadyStates.js
@@ -0,0 +1,5 @@
+UNSENT = 0; // initial state
+OPENED = 1; // open called
+HEADERS_RECEIVED = 2; // response headers received
+LOADING = 3; // response is loading (a data packet is received)
+DONE = 4; // request complete
