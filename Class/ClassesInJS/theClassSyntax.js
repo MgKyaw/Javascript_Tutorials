2e1632d34ab270d29fd8054b715596181c66035e
MgKyaw <mgkyaw20112@gmail.com>
2025-12-09 23:20:37 +0700

    feat: Rewrite class User in pure functions

--- Class/ClassesInJS/theClassSyntax.js
+++ Class/ClassesInJS/theClassSyntax.js
@@ -43,3 +43,21 @@ alert(User.prototype.sayHi); // the code of the sayHi method
 
 // there are exactly two methods in the prototype
 alert(Object.getOwnPropertyNames(User.prototype)); // constructor, sayHi
+
+// rewriting class User in pure functions
+
+// 1. Create constructor function
+function User(name) {
+  this.name = name;
+}
+// a function prototype has "constructor" property by default,
+// so we don't need to create it
+
+// 2. Add the method to prototype
+User.prototype.sayHi = function() {
+  alert(this.name);
+};
+
+// Usage:
+let user2 = new User("John");
+user2.sayHi();
