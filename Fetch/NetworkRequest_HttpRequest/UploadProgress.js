dd8a8ff9eb19ad224a0b056b4ed0647c12ac1b42
MgKyaw <mgkyaw20112@gmail.com>
2025-12-09 23:10:51 +0700

    feat: Add UploadProgress

---
+++ Fetch/NetworkRequest_HttpRequest/UploadProgress.js
@@ -0,0 +1,9 @@
+// loadstart – upload started.
+// progress – triggers periodically during the upload.
+// abort – upload aborted.
+// error – non-HTTP error.
+// load – upload finished successfully.
+// timeout – upload timed out (if timeout property is set).
+// loadend – upload finished with either success or error.
+
+
