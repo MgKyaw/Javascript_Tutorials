e904b22f8b73d3f68c954c9619efcd0d1e6eb7a5
MgKyaw <mgkyaw20112@gmail.com>
2025-12-10 23:39:09 +0700

    feat: assign prop values

--- Class/ClassesInJS/addPropToUserClass.js
+++ Class/ClassesInJS/addPropToUserClass.js
@@ -15,3 +15,10 @@ class User {
 let user = new User();
 alert(user.name); // John
 alert(User.prototype.name); // undefined
+
+class User {
+  name = prompt("Name, please?", "John");
+}
+
+let user2 = new User();
+alert(user2.name); // John
