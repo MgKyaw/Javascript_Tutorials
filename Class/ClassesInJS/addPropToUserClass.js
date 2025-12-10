dd35708371090e8ebeea5de8fabf82909da52fd1
MgKyaw <mgkyaw20112@gmail.com>
2025-12-10 23:38:18 +0700

    feat: Get prop of class

--- Class/ClassesInJS/addPropToUserClass.js
+++ Class/ClassesInJS/addPropToUserClass.js
@@ -7,3 +7,11 @@ class User {
 }
 
 new User().sayHi(); // Hello, John!
+
+class User {
+  name = "John";
+}
+
+let user = new User();
+alert(user.name); // John
+alert(User.prototype.name); // undefined
