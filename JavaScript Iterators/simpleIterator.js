91b0a8a4c3762543026cdb4096e80ab30948241f
MgKyaw <mgkyaw20112@gmail.com>
2025-12-12 23:15:57 +0700

    feat: Call iter.next multiple times

--- JavaScript Iterators/simpleIterator.js
+++ JavaScript Iterators/simpleIterator.js
@@ -1,3 +1,9 @@
 var nums = [1, 5, 16];
 
-iter.next();
+iter.next(); // {value: 1, done: false}
+
+iter.next(); // {value: 5, done: false}
+
+iter.next(); // {value: 16, done: false}
+
+iter.next(); // {value: undefined, done: true}
