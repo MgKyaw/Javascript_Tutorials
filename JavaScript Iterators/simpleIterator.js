8495330b9e42e403446da426ff44eeabd3367769
MgKyaw <mgkyaw20112@gmail.com>
2025-12-12 23:20:01 +0700

    feat: Create makeIterator function

--- JavaScript Iterators/simpleIterator.js
+++ JavaScript Iterators/simpleIterator.js
@@ -11,3 +11,14 @@ iter.next(); // {value: 16, done: false}
 iter.next(); // {value: undefined, done: true}
 iter.next(); // {value: undefined, done: true}
 iter.next(); // {value: undefined, done: true}
+
+function makeIterator(array) {
+    var i = 0;
+    return {
+        next: function() {
+            if (i > arr.length - 1)
+                return {value: undefined, done: true}
+            return {value: arr[i++], done: false}
+        }
+    }
+}
