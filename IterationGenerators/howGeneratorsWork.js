f9d89d0ed2aec7c881726eee45949fc7013e2185
MgKyaw <mgkyaw20112@gmail.com>
2025-12-13 22:02:13 +0700

    feat: Add logs for each yield return

--- IterationGenerators/howGeneratorsWork.js
+++ IterationGenerators/howGeneratorsWork.js
@@ -1,8 +1,14 @@
 function* sequence() {
-   console.log("Started!");
+   console.log("First!");
    yield 1;
+
+   console.log("Second!");
    yield 3;
+
+   console.log("Third!");
    yield 5;
+
+   console.log("Done!");
 }
 
-var seq = sequence(); // nothing logged
+var seq = sequence();
