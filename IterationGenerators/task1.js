a13610ee8f43a1d91d2ccacda5abec2610df6913
MgKyaw <mgkyaw20112@gmail.com>
2025-12-14 15:34:08 +0700

    feat: Add output results

--- IterationGenerators/task1.js
+++ IterationGenerators/task1.js
@@ -11,3 +11,9 @@ seq.next(15);
 seq.next(60);
 seq.next(32);
 seq.next(4);
+
+// Output
+// {value: 20, done: false}
+// {value: 65, done: false}
+// {value: 37, done: false}
+// {value: 9, done: false}
