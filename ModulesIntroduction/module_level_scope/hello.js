943ebee08bd2b1960e7fdc390271065dc33b8217
MgKyaw <mgkyaw20112@gmail.com>
2025-12-15 23:08:05 +0700

    feat: Add the correct way to export

--- ModulesIntroduction/module_level_scope/hello.js
+++ ModulesIntroduction/module_level_scope/hello.js
@@ -1,1 +1,6 @@
 alert(user); // no such variable (each module has independent variables)
+
+// Correct way to export variable
+import {user} from './user.js';
+
+document.body.innerHTML = user; // John
