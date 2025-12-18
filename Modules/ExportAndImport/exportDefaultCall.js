138abc663f604d6ad65e8f18a75d1de3757f50bb
MgKyaw <mgkyaw20112@gmail.com>
2025-12-18 21:41:29 +0700

    feat: Add sample usage between Named and default export

--- Modules/ExportAndImport/exportDefaultCall.js
+++ Modules/ExportAndImport/exportDefaultCall.js
@@ -2,3 +2,7 @@
 import User from './user.js'; // not {User}, just User
 
 new User('John');
+
+// Named export	                    Default export
+// export class User {...}	        export default class User {...}
+// import {User} from ...	        import User from ...
