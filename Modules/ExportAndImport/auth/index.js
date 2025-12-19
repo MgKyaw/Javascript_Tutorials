b4e6736438e09eb6bb75c07ebd7f730ba71814be
MgKyaw <mgkyaw20112@gmail.com>
2025-12-19 20:11:59 +0700

    feat: Add auth/index.js

---
+++ Modules/ExportAndImport/auth/index.js
@@ -0,0 +1,11 @@
+// 📁 auth/index.js
+
+// import login/logout and immediately export them
+import {login, logout} from './helpers.js';
+export {login, logout};
+
+// import default as User and export it
+import User from './user.js';
+export {User};
+
+///////
