bb463c6ae267beff5f3c3f212c2d19d6ebd4cdab
MgKyaw <mgkyaw20112@gmail.com>
2025-12-19 20:07:34 +0700

    feat: Update main.js

--- Modules/ExportAndImport/main.js
+++ Modules/ExportAndImport/main.js
@@ -1,4 +1,9 @@
 // 📁 main.js
-import {default as User, sayHi} from './user.js';
+// import {default as User, sayHi} from './user.js';
 
+// new User('John');
+
+import * as user from './user.js';
+
+let User = user.default; // the default export
 new User('John');
