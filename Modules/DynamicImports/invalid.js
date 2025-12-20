41074d1984c2b83ba0ca8cf217c636f5bf4d03bd
MgKyaw <mgkyaw20112@gmail.com>
2025-12-20 23:12:29 +0700

    feat: Add second invalid import

--- Modules/DynamicImports/invalid.js
+++ Modules/DynamicImports/invalid.js
@@ -1,2 +1,12 @@
 // First, we can’t dynamically generate any parameters of import.
 // import ... from getModuleName(); // Error, only from "string" is allowed
+
+// Second, we can’t import conditionally or at run-time:
+
+// if(...) {
+//   import ...; // Error, not allowed!
+// }
+
+// {
+//   import ...; // Error, we can't put import in any block
+// }
