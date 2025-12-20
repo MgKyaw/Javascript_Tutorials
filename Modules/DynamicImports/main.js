c19019bccaa5abf1f64b49782b6b5f6799940d87
MgKyaw <mgkyaw20112@gmail.com>
2025-12-20 23:17:00 +0700

    feat: Import say.js with default

--- Modules/DynamicImports/main.js
+++ Modules/DynamicImports/main.js
@@ -2,3 +2,9 @@ let {hi, bye} = await import('./say.js');
 
 hi();
 bye();
+
+let obj = await import('./say.js');
+let say = obj.default;
+// or, in one line: let {default: say} = await import('./say.js');
+
+say();
