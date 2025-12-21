f6224f142c5daa194eecd45455d400259b8239ec
MgKyaw <mgkyaw20112@gmail.com>
2025-12-21 23:13:21 +0700

    feat: Add imports

--- Modules/ESM/FullESMModules/main.js
+++ Modules/ESM/FullESMModules/main.js
@@ -1,6 +1,13 @@
 import { name as circleName } from "https://example.com/shapes/circle.js";
 import { name as squareName, draw } from "./shapes/square.js";
 
+// Bare module names as module specifiers
+import { name as squareNameOne } from "shapes";
+import { name as squareNameTwo } from "shapes/square";
+
+// Remap a URL to another URL
+import { name as squareNameThree } from "https://example.com/shapes/square.js";
+
 const myCanvas = create("myCanvas", document.body, 480, 320);
 const reportList = createReportList(myCanvas.id);
 
