5e297f88e51078b2863f09f92e24325b8ba4becf
MgKyaw <mgkyaw20112@gmail.com>
2025-12-23 22:30:44 +0700

    feat: Add import

---
+++ Modules/ESM/FullESMModules/import.js
@@ -0,0 +1,3 @@
+import { name, draw, reportArea, reportPerimeter } from "./modules/square.js";
+import { name, draw, reportArea, reportPerimeter } from "./modules/circle.js";
+import { name, draw, reportArea, reportPerimeter } from "./modules/triangle.js";
