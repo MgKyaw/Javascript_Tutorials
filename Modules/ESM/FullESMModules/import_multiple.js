d05980994af998d5d68ca5b6ceefa1da2eba4e84
MgKyaw <mgkyaw20112@gmail.com>
2025-12-24 20:41:43 +0700

    feat" Use square.draw

--- Modules/ESM/FullESMModules/import_multiple.js
+++ Modules/ESM/FullESMModules/import_multiple.js
@@ -3,3 +3,7 @@ import * as Canvas from "./modules/canvas.js";
 import * as Square from "./modules/square.js";
 import * as Circle from "./modules/circle.js";
 import * as Triangle from "./modules/triangle.js";
+
+const square = Square.draw(myCanvas.ctx, 50, 50, 100, "blue");
+Square.reportArea(square.length, reportList);
+Square.reportPerimeter(square.length, reportList);
