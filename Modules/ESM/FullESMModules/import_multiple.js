f0d86aac085e055070e3f5752440ab5b8a114ff2
MgKyaw <mgkyaw20112@gmail.com>
2025-12-24 20:41:22 +0700

    feat: Add import multiple

---
+++ Modules/ESM/FullESMModules/import_multiple.js
@@ -0,0 +1,5 @@
+import * as Canvas from "./modules/canvas.js";
+
+import * as Square from "./modules/square.js";
+import * as Circle from "./modules/circle.js";
+import * as Triangle from "./modules/triangle.js";
