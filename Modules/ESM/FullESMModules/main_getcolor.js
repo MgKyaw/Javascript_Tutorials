37f396e0e1905ba816141675fcbbe27411821a5c
MgKyaw <mgkyaw20112@gmail.com>
2025-12-25 09:45:20 +0700

    feat: Use colors for color

--- Modules/ESM/FullESMModules/main_getcolor.js
+++ Modules/ESM/FullESMModules/main_getcolor.js
@@ -3,4 +3,29 @@ import { Canvas } from "./modules/canvas.js";
 
 const circleBtn = document.querySelector(".circle");
 
-// …
+const square = new Module.Square(
+  myCanvas.ctx,
+  myCanvas.listId,
+  50,
+  50,
+  100,
+  colors.blue,
+);
+
+const circle = new Module.Circle(
+  myCanvas.ctx,
+  myCanvas.listId,
+  75,
+  200,
+  100,
+  colors.green,
+);
+
+const triangle = new Module.Triangle(
+  myCanvas.ctx,
+  myCanvas.listId,
+  100,
+  75,
+  190,
+  colors.yellow,
+);
