731c7b92b90320cc503e890fa3dc013ee94a2f94
MgKyaw <mgkyaw20112@gmail.com>
2025-12-29 21:41:07 +0700

    feat: Add title "file: URLs"

--- Modules/ESM/NotejsESModules/InformSpecifiers.js
+++ Modules/ESM/NotejsESModules/InformSpecifiers.js
@@ -1,4 +1,5 @@
 // import Specifiers
 
+// file: URLs
 import './foo.mjs?query=1'; // loads ./foo.mjs with query of "?query=1"
 import './foo.mjs?query=2'; // loads ./foo.mjs with query of "?query=2"
