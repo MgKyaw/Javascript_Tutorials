1bb2fe0193ee5575d98d9af4680bc7acc25b65e3
MgKyaw <mgkyaw20112@gmail.com>
2025-12-29 21:50:48 +0700

    feat: Add Built-in modules

---
+++ Modules/ESM/NotejsESModules/Built-in_Modules.js
@@ -0,0 +1,7 @@
+// Built-in modules
+
+// Built-in modules provide named exports of their public API. 
+// A default export is also provided which is the value of the CommonJS exports. 
+// The default export can be used for, among other things, modifying the named exports. 
+// Named exports of built-in modules are updated only 
+// by calling module.syncBuiltinESMExports().
