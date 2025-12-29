cd56a3c485a916bf70754bc9ad584ece392f9a89
MgKyaw <mgkyaw20112@gmail.com>
2025-12-29 21:49:32 +0700

    feat: Add Remarks for Import Attributes

--- Modules/ESM/NotejsESModules/ImportAttributes.js
+++ Modules/ESM/NotejsESModules/ImportAttributes.js
@@ -6,4 +6,10 @@ import fooData from './foo.json' with { type: 'json' };
 
 const { default: barData } =
   await import('./bar.json', { with: { type: 'json' } });
-  
+
+// Node.js only supports the type attribute, for which it supports the following values:
+
+// Attribute type	Needed for
+// 'json'	JSON modules
+
+// The type: 'json' attribute is mandatory when importing JSON modules.
