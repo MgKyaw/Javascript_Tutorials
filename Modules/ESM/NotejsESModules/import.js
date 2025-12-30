e7d83039ae23889de96093dd71d8493dcc53da7e
MgKyaw <mgkyaw20112@gmail.com>
2025-12-30 22:57:57 +0700

    feat: Add import expression

---
+++ Modules/ESM/NotejsESModules/import.js
@@ -0,0 +1,4 @@
+// import() expressions
+
+// Dynamic import() provides an asynchronous way to import modules. It is supported in both CommonJS and ES modules, and can be used to load both CommonJS and ES modules.
+
