8e09059973f190a08e1a0c57f4bf0b356d8323dd
MgKyaw <mgkyaw20112@gmail.com>
2025-12-30 22:58:19 +0700

    feat: Add import.meta

--- Modules/ESM/NotejsESModules/import.js
+++ Modules/ESM/NotejsESModules/import.js
@@ -2,3 +2,6 @@
 
 // Dynamic import() provides an asynchronous way to import modules. It is supported in both CommonJS and ES modules, and can be used to load both CommonJS and ES modules.
 
+// import.meta#
+// Type: <Object>
+// The import.meta meta property is an Object that contains the following properties. It is only supported in ES modules.
