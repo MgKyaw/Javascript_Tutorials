c4879ee81657ae3a3a1a5649043ec10096686feb
MgKyaw <mgkyaw20112@gmail.com>
2025-12-30 22:59:18 +0700

    feat: Add import.meta.filename

--- Modules/ESM/NotejsESModules/import.js
+++ Modules/ESM/NotejsESModules/import.js
@@ -12,3 +12,10 @@
 // This is the same as the path.dirname() of the import.meta.filename.
 
 // Caveat: only present on file: modules.
+
+// import.meta.filename#
+// History
+// Type: <string> The full absolute path and filename of the current module, with symlinks resolved.
+// This is the same as the url.fileURLToPath() of the import.meta.url.
+
+// Caveat only local modules support this property. Modules not using the file: protocol will not provide it.
