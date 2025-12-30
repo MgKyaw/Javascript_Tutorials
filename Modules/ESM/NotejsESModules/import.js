bd4f5407519d4b51fb73b8b619f40aa1a79cb678
MgKyaw <mgkyaw20112@gmail.com>
2025-12-30 22:58:45 +0700

    feat: Add import.meta.dirname

--- Modules/ESM/NotejsESModules/import.js
+++ Modules/ESM/NotejsESModules/import.js
@@ -5,3 +5,10 @@
 // import.meta#
 // Type: <Object>
 // The import.meta meta property is an Object that contains the following properties. It is only supported in ES modules.
+
+// import.meta.dirname#
+// History
+// Type: <string> The directory name of the current module.
+// This is the same as the path.dirname() of the import.meta.filename.
+
+// Caveat: only present on file: modules.
