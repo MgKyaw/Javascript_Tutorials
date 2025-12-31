ca9b94ff10f278e82dd39bcd06a4b1621ae00e01
MgKyaw <mgkyaw20112@gmail.com>
2025-12-31 18:02:53 +0700

    feat: Add import wasm

---
+++ Modules/ESM/NotejsESModules/wasm_import.js
@@ -0,0 +1,6 @@
+import source libraryModule from './library.wasm';
+
+const instance1 = await WebAssembly.instantiate(libraryModule, importObject1);
+
+const instance2 = await WebAssembly.instantiate(libraryModule, importObject2);
+
