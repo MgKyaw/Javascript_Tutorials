dbfd34111cf3c868b7e5324858e533feed31d336
MgKyaw <mgkyaw20112@gmail.com>
2026-01-01 16:19:00 +0700

    feat: Add output of running the file

--- Modules/CommonJS/import_export/functions.js
+++ Modules/CommonJS/import_export/functions.js
@@ -1,3 +1,28 @@
 const add = (num1, num2) => num1 + num2;
 
 console.log(module);
+
+// node functions.js
+// output
+// {
+//   id: '.',
+//   path: 'D:\\project\\javascript\\Javascript_Tutorials\\Modules\\CommonJS\\import_export',
+//   exports: {},
+//   filename: 'D:\\project\\javascript\\Javascript_Tutorials\\Modules\\CommonJS\\import_export\\functions.js',    
+//   loaded: false,
+//   children: [],
+//   paths: [
+//     'D:\\project\\javascript\\Javascript_Tutorials\\Modules\\CommonJS\\import_export\\node_modules',
+//     'D:\\project\\javascript\\Javascript_Tutorials\\Modules\\CommonJS\\node_modules',
+//     'D:\\project\\javascript\\Javascript_Tutorials\\Modules\\node_modules',
+//     'D:\\project\\javascript\\Javascript_Tutorials\\node_modules',
+//     'D:\\project\\javascript\\node_modules',
+//     'D:\\project\\node_modules',
+//     'D:\\node_modules'
+//   ],
+//   [Symbol(kIsMainSymbol)]: true,
+//   [Symbol(kIsCachedByESMLoader)]: false,
+//   [Symbol(kURL)]: undefined,
+//   [Symbol(kFormat)]: undefined,
+//   [Symbol(kIsExecuting)]: true
+// }
