32273333d1b22d9cc83f36181a1c7ee61b5616bc
MgKyaw <mgkyaw20112@gmail.com>
2026-01-01 16:20:44 +0700

    feat: Add output of the functions.js

--- Modules/CommonJS/import_export/functions.js
+++ Modules/CommonJS/import_export/functions.js
@@ -30,3 +30,26 @@ console.log(module);
 module.exports.add = add;
 
 console.log(module);
+// Output
+// {
+//   id: '.',
+//   path: 'D:\\project\\javascript\\Javascript_Tutorials\\Modules\\CommonJS\\import_export',
+//   exports: { add: [Function: add] },
+//   filename: 'D:\\project\\javascript\\Javascript_Tutorials\\Modules\\CommonJS\\import_export\\functions.js',    
+//   loaded: false,
+//   children: [],
+//   paths: [
+//     'D:\\project\\javascript\\Javascript_Tutorials\\Modules\\CommonJS\\import_export\\node_modules',
+//     'D:\\project\\javascript\\Javascript_Tutorials\\Modules\\CommonJS\\node_modules',
+//     'D:\\project\\javascript\\Javascript_Tutorials\\Modules\\node_modules',
+//     'D:\\project\\javascript\\Javascript_Tutorials\\node_modules',
+//     'D:\\project\\javascript\\node_modules',
+//     'D:\\project\\node_modules',
+//     'D:\\node_modules'
+//   ],
+//   [Symbol(kIsMainSymbol)]: true,
+//   [Symbol(kIsCachedByESMLoader)]: false,
+//   [Symbol(kURL)]: undefined,
+//   [Symbol(kFormat)]: undefined,
+//   [Symbol(kIsExecuting)]: true
+// }
