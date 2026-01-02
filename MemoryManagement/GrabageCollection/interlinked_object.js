9e016956f17e9e7b3d2512edeca1ff6822ed4340
MgKyaw <mgkyaw20112@gmail.com>
2026-01-02 17:54:22 +0700

    feat: Add description when removing two references from interlined objects

--- MemoryManagement/GrabageCollection/interlinked_object.js
+++ MemoryManagement/GrabageCollection/interlinked_object.js
@@ -13,3 +13,10 @@ let family = marry({
 }, {
   name: "Ann"
 });
+
+delete family.father;
+delete family.mother.husband;
+
+// It’s not enough to delete only one of these two references, because all objects would still be reachable.
+
+// But if we delete both, then we can see that John has no incoming reference any more:
