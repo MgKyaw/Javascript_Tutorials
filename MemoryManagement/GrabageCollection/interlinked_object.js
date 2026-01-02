2042f7cff128eeffbe861718fd594e68b3f26f46
MgKyaw <mgkyaw20112@gmail.com>
2026-01-02 17:56:04 +0700

    feat: Add example for unreachable island

--- MemoryManagement/GrabageCollection/interlinked_object.js
+++ MemoryManagement/GrabageCollection/interlinked_object.js
@@ -20,3 +20,10 @@ delete family.mother.husband;
 // It’s not enough to delete only one of these two references, because all objects would still be reachable.
 
 // But if we delete both, then we can see that John has no incoming reference any more:
+
+//Unreachable island
+//It is possible that the whole island of interlinked objects becomes unreachable and is removed from the memory.
+
+//The source object is the same as above. Then:
+
+family = null;
