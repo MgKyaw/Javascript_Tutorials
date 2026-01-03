f6d20a7c62536bc4bb99b4d11d3dbc20bf54731a
MgKyaw <mgkyaw20112@gmail.com>
2026-01-03 09:42:38 +0700

    feat: Add allocation with new values and objects

--- MemoryManagement/MemoryManagement/allocation.js
+++ MemoryManagement/MemoryManagement/allocation.js
@@ -22,3 +22,15 @@ someElement.addEventListener("click", () => {
 const d = new Date(); // allocates a Date object
 
 const e = document.createElement("div"); // allocates a DOM element
+
+const s1 = "string";
+const s2 = s.substring(0, 3); // s2 is a new string
+// Since strings are immutable values,
+// JavaScript may decide to not allocate memory,
+// but just store the [0, 3] range.
+
+const a1 = ["yeah yeah", "no no"];
+const a2 = ["generation", "no no"];
+const a3 = a.concat(a2);
+// new array with 4 elements being
+// the concatenation of a and a2 elements.
