7c5da1485e9a2a7bae80a0880efe0d22e4bbcf02
MgKyaw <mgkyaw20112@gmail.com>
2026-01-04 09:35:21 +0700

    fix: Replace single code

--- DevTools/DebuggingMemoryLeaks/Catching/detachedDOMNodes.js
+++ DevTools/DebuggingMemoryLeaks/Catching/detachedDOMNodes.js
@@ -1,11 +1,11 @@
-var node = document.createElement(‘a’);
+var node = document.createElement('a');
 node.id = 'id1';
 document.body.appendChild(node);
 var main = {
-   Id: document.getElementById(‘id1’)
+   Id: document.getElementById('id1')
 }
 function removeElement(){
-   document.body.removeChild(document.getElementById(‘id1’));
+   document.body.removeChild(document.getElementById('id1'));
 }
-// The ‘this’ here refers to window object and hence this variable will be created in the window.
+// The 'this’ here refers to window object and hence this variable will be created in the window.
 removeElement();
