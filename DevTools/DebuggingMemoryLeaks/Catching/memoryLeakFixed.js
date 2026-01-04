73aa04a719aeca51cd057f696b82636e4a168b5e
MgKyaw <mgkyaw20112@gmail.com>
2026-01-04 09:41:47 +0700

    feat: Add fixed version of memory leak

---
+++ DevTools/DebuggingMemoryLeaks/Catching/memoryLeakFixed.js
@@ -0,0 +1,16 @@
+var bool = false;
+function grow(){
+  var x = [];
+  x.push(new Array(100000).join('a'));
+  
+  if(bool){
+   setTimeout(grow, 1000);
+  }
+}
+function start(){
+  grow();
+  bool = true;
+}
+function stop(){
+  bool = false;
+}
